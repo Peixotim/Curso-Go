0b312d4e64535e2cd9e83a213eb19784815d8251
Peixotim <pedropeixotovz@gmail.com>
2025-10-06 09:40:27 -0300

    Feat: Adiciona Aula 5

---
+++ Aula 5/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//O tipo bool sem ser instanciado é iniciado como false
+	var tipoBool bool
+
+	var boolTrue bool = true
+	var boolFalse bool = false
+
+	fmt.Println(tipoBool)
+	fmt.Println("- - -")
+	fmt.Println(boolTrue)
+	fmt.Println(boolFalse)
+}
