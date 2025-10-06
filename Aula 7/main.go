454a105a8a28aeb5311ae209fadf1c88c4983a6e
Peixotim <pedropeixotovz@gmail.com>
2025-10-06 09:54:00 -0300

    feat: Adiciona a Aula 7

---
+++ Aula 7/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var gavetas [4]string
+	gavetas[0] = "Gaveta Vazia !"
+	gavetas[1] = "Meias"
+	gavetas[2] = "Blusa"
+	gavetas[3] = "Gaveta Vazia !"
+
+	fmt.Println(gavetas[0])
+	fmt.Println(gavetas)
+}
